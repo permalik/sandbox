f22e878482237f141b4d3271a3f1cad104a20aae
permalik <permalik@protonmail.com>
2024-07-21 23:04:37 -0500

    feat impl cmake and script build

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int insertion_sort(int arr[], int len) {
+void insertion_sort(int arr[], int len) {
   for (int i = 1; i < len; i++) {
     int tmp = arr[i];
     int j = i - 1;
@@ -12,16 +12,24 @@ int insertion_sort(int arr[], int len) {
 
     arr[j + 1] = tmp;
   };
-  return 0;
 }
 
 int main() {
+  printf("INSERTION SORT:\n");
   int length = 10;
   int array[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
 
-  insertion_sort(array, length);
+  printf("unsorted array:\n");
   for (int i = 0; i < length; i++) {
-    printf("%d\n", array[i]);
+    printf("%d ", array[i]);
   }
+
+  insertion_sort(array, length);
+
+  printf("\nsorted array:\n");
+  for (int i = 0; i < length; i++) {
+    printf("%d ", array[i]);
+  }
+
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-rm main
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 arg1 [arg2 .. argN]"
+    exit 1
+fi
 
-gcc -o main main.c
+build_insertion_sort="/Users/au4/git/dsa_c/insertion_sort/build"
 
-./main
+if [ "$1" == "insertion_sort" ]; then
+    if [ -d "$build_insertion_sort" ]; then
+        echo "tearing down build.."
+        rm -rf /Users/au4/git/dsa_c/insertion_sort/build
+        cd /Users/au4/git/dsa_c/insertion_sort
+        echo "creating build directory.."
+        mkdir build
+        cd /Users/au4/git/dsa_c/insertion_sort/build
+        echo "generating build assets.."
+        cmake ..
+        echo "compiling project.."
+        cmake --build .
+        echo "executing project.."
+        ./insertion_sort
+    else
+        cd /Users/au4/git/dsa_c/insertion_sort
+        echo "creating build directory.."
+        mkdir build
+        cd /Users/au4/git/dsa_c/insertion_sort/build
+        echo "generating build assets.."
+        cmake ..
+        echo "compiling project.."
+        cmake --build .
+        echo "executing project.."
+        ./insertion_sort
+    fi
+fi
