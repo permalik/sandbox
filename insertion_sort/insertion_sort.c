910e0be7979cb04cff51bd0da858fefe3cf3c2b6
permalik <permalik@protonmail.com>
2024-07-22 06:48:03 -0500

    refactor; chore derive length of array_one update todo

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// TODO: impl all insertion sort concepts
+// TODO: impl recursive strategy
 // TODO: impl test
 // TODO: impl complexity analysis
 
-void insertion_sort(int arr[], int len) {
+void iterative_insertion_sort(int arr[], int len) {
   for (int i = 1; i < len; i++) {
     int tmp = arr[i];
     int j = i - 1;
@@ -19,21 +19,25 @@ void insertion_sort(int arr[], int len) {
 }
 
 int main() {
-  printf("INSERTION SORT:\n");
-  int length = 10;
-  int array[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
+  printf("ITERATIVE INSERTION SORT:\n");
+  int array_one[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
+  size_t size_in_bytes_one = sizeof(array_one);
+  size_t item_size_one = sizeof(array_one[0]);
+  size_t length_one = size_in_bytes_one / item_size_one;
 
   printf("unsorted array:\n");
-  for (int i = 0; i < length; i++) {
-    printf("%d ", array[i]);
+  for (int i = 0; i < length_one; i++) {
+    printf("%d ", array_one[i]);
   }
 
-  insertion_sort(array, length);
+  iterative_insertion_sort(array_one, length_one);
 
   printf("\nsorted array:\n");
-  for (int i = 0; i < length; i++) {
-    printf("%d ", array[i]);
+  for (int i = 0; i < length_one; i++) {
+    printf("%d ", array_one[i]);
   }
+
+  printf("\nRECURSIVE INSERTION SORT:\n");
 
   return 0;
 }
