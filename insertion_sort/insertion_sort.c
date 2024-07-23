17572922135b63ef53d0e61f3fd8b25d6f9e2710
permalik <permalik@protonmail.com>
2024-07-22 22:59:03 -0500

    waypoint

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -18,6 +18,23 @@ void iterative_insertion_sort(int arr[], int len) {
   };
 }
 
+void recursive_insertion_sort(int arr[], int n) {
+  if (n <= 1) {
+    return;
+  }
+
+  recursive_insertion_sort(arr, n - 1);
+
+  int last = arr[n - 1];
+  int j = n - 2;
+
+  while (j >= 0 && arr[j] < last) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = last;
+}
+
 int main() {
   printf("ITERATIVE INSERTION SORT:\n");
   int array_one[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
@@ -38,6 +55,21 @@ int main() {
   }
 
   printf("\nRECURSIVE INSERTION SORT:\n");
+  int array_two[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
+  size_t size_in_bytes_two = sizeof(array_two);
+  size_t item_size_two = sizeof(array_two[0]);
+  size_t length_two = size_in_bytes_two / item_size_two;
 
+  printf("unsorted array:\n");
+  for (int i = 0; i < length_two; i++) {
+    printf("%d ", array_two[i]);
+  }
+
+  recursive_insertion_sort(array_two, length_two);
+
+  printf("\nsorted array:\n");
+  for (int i = 0; i < length_one; i++) {
+    printf("%d ", array_two[i]);
+  }
   return 0;
 }
