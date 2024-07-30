50387dd2b58cbdeb213dd333f51f7e33c108768e
permalik <permalik@protonmail.com>
2024-07-30 07:02:23 -0500

    feat impl mulidimensional_array

--- dsa/array/src/array.c
+++ dsa/array/src/array.c
@@ -43,8 +43,24 @@ void dynamic_array() {
   free(arr);
 }
 
+void multidimensional_array() {
+  int arr[2][3][2] = {{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}};
+
+  printf("\nMULTIDIMENSIONAL ARRAY:\n");
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 2; k++) {
+        printf("%d ", arr[i][j][k]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+}
+
 int main() {
   static_array();
   dynamic_array();
+  multidimensional_array();
   return 0;
 }
