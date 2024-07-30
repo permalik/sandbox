067abf43382743cb7d8e669ef34112983de1c7e2
permalik <permalik@protonmail.com>
2024-07-30 00:03:26 -0500

    feat;refactor update insertion_sort printing complete static_array, dynamic_array

--- dsa/array/src/array.c
+++ dsa/array/src/array.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// TODO: impl array concepts
+// TODO: impl multidimensional array
+// TODO: impl array of pointers
 
 void static_array() {
   int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int elm_len = sizeof(arr[0]);
+  int elm_len = sizeof(int);
   int arr_len = sizeof(arr) / elm_len;
 
   printf("STATIC ARRAY:\n");
@@ -17,7 +19,32 @@ void static_array() {
   }
 }
 
+void dynamic_array() {
+  int *arr;
+  int arr_len = 10;
+
+  arr = (int *)malloc(arr_len * sizeof(int));
+
+  printf("\nINIT DYNAMIC ARRAY (10):\n");
+  for (int i = 0; i < arr_len; i++) {
+    arr[i] = i + 1;
+    printf("%d ", arr[i]);
+  }
+
+  arr_len = 20;
+  arr = (int *)realloc(arr, arr_len * sizeof(int));
+
+  printf("\nRESIZE DYNAMIC ARRAY (20):\n");
+  for (int i = 0; i < arr_len; i++) {
+    arr[i] = i + 1;
+    printf("%d ", arr[i]);
+  }
+
+  free(arr);
+}
+
 int main() {
   static_array();
+  dynamic_array();
   return 0;
 }
--- dsa/insertion_sort/insertion_sort.c
+++ dsa/insertion_sort/insertion_sort.c
@@ -45,20 +45,16 @@ int main() {
   size_t length_one = size_in_bytes_one / item_size_one;
 
   printf("unsorted array:\n");
-  printf("{ ");
   for (int i = 0; i < length_one; i++) {
     printf("%d ", array_one[i]);
   }
-  printf("}");
 
   iterative_insertion_sort(array_one, length_one);
 
   printf("\nsorted array:\n");
-  printf("{ ");
   for (int i = 0; i < length_one; i++) {
     printf("%d ", array_one[i]);
   }
-  printf("}");
 
   printf("\nRECURSIVE INSERTION SORT:\n");
   int array_two[10] = {5, 7, 3, 6, 1, 4, 0, 9, 8, 2};
@@ -67,20 +63,16 @@ int main() {
   size_t length_two = size_in_bytes_two / item_size_two;
 
   printf("unsorted array:\n");
-  printf("{ ");
   for (int i = 0; i < length_one; i++) {
     printf("%d ", array_two[i]);
   }
-  printf("}");
 
   recursive_insertion_sort(array_two, length_two);
 
   printf("\nsorted array:\n");
-  printf("{ ");
   for (int i = 0; i < length_one; i++) {
     printf("%d ", array_two[i]);
   }
-  printf("}");
 
   return 0;
 }
